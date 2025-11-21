4176bf326a287db2e3631bf1b4377b7368bac86d
FIKY 0911 <fiky0911@gmail.com>
2025-11-21 21:39:01 +0700

    Penambahan Project dan Skill

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -1,9 +1,12 @@
-import HeroImage from "../assets/hero/heroimage.webp";
-import Nextjs from "../assets/tools/next-js-tools.webp";
-import TypeScript from "../assets/tools/Typescript-tools.webp";
-import Clerk from "../assets/tools/clrek-tools.webp";
-import Tailwind from "../assets/tools/tailwind-css-tools.webp";
-import Project1 from "../assets/project/project1.webp";
+import HeroImage from "../assets/hero/heroimage.webp"
+import Html from "../assets/tools/Html-tools.webp"
+import Css from "../assets/tools/Css-tools.webp"
+import Nextjs from "../assets/tools/next-js-tools.webp"
+import TypeScript from "../assets/tools/Typescript-tools.webp"
+import Clerk from "../assets/tools/clrek-tools.webp"
+import Tailwind from "../assets/tools/tailwind-css-tools.webp"
+import Project1 from "../assets/project/project1.webp"
+import Project2 from "../assets/project/project2.webp"
 
 export const Image = {
   HeroImage,
@@ -14,7 +17,7 @@ export const listTools = [
     id: 1,
     name: "Nextjs",
     image: Nextjs,
-    ket: "Framework",
+    ket: "Framework JS dan TS",
   },
   {
     id: 2,
@@ -32,7 +35,19 @@ export const listTools = [
     id: 4,
     image: Tailwind,
     name: "Tailwind",
-    ket: "Framework",
+    ket: "Framework Css",
+  },
+  {
+    id:5,
+    image: Html,
+    name: "Html",
+    ket: "Hyper Text Markup Language",
+  },
+  {
+    id:6,
+    image: Css,
+    name: "Css",
+    ket: "Cascading Style Sheets",
   }
 ];
 
@@ -44,5 +59,13 @@ export const listProject = [
     link: "https://grocerystore-rpl-kel13.vercel.app/", // ← hapus spasi ekstra di akhir
     description: "Ini adalah project e-commerce UAS-RPL yang dibuat menggunakan React dan Tailwind CSS.",
     tools: ["Next js", "TypeScript", "Clerk", "Tailwind"],
+  },
+  {
+    id:2,
+    title: "Coffeshop",
+    images: Project2,
+    link: "https://tugas-bahasa-pemrograman.vercel.app/",
+    description: "Ini adalah project Bahasa Pemrograman saya bersama temen saya",
+    tools: ["Html", "Css", "Javascript"],
   }
 ];
